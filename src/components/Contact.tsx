7400f77355d6695fd5b8785120ff4d9d323fa81b
Dominic Gomes <gomes_dominic@outlook.com>
2025-06-04 18:16:12 +0600

    update design

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -51,7 +51,7 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact" className="mt-20">
+    <section id="contact" className="mt-20 py-10">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <SectionHeader
           title="Get In "
